668f152d027e803e176cfc2f3849d282626455a1
ribaorsi <ribaorsi@gmail.com>
2021-10-26 23:41:18 +0200

    code checked

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-function fastLog(text){
-    console.log(text);
-    return "muhaha";
-}
-
-function buttonClicked(event){
-//   console.log(event.target);
-
-//   console.log(event.target.classList);
-
+function toggleClicked (event) {
     event.target.classList.toggle("clicked");
-
-    const fastLogResult = fastLog("Ezt a szöveget szeretném kilogoltatni");
-
-    console.log(fastLogResult);
+    document.getElementById("nav").classList.toggle("clicked");
+    console.log("the button is clicked")
 }
+
 function loadEvent() {
     console.log('the page has loaded');
 
+    document.getElementById('menu-btn').addEventListener('click', toggleClicked); 
 
-    document.getElementById('menu-btn').addEventListener('click', buttonClicked); 
-//  console.log(document.getElementById('menu-btn'));
 }
 
 window.addEventListener("load", loadEvent);
+
+//body kapjon id-t, a click esemény a gombon hívódjon meg, a body kapja a click class nevet, és működjön a CSS
